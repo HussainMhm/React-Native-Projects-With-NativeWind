7eab2f6eecf83bca11b60c21b3420ca7e77bae65
hussain <hussainabdullatif@marun.edu.tr>
2023-11-18 22:46:13 +0300

    Added React Navigation

--- Weather App/App.js
+++ Weather App/App.js
@@ -1,11 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import Navigation from "./navigation";
 
 export default function App() {
-    return (
-        <View className="flex-1 items-center justify-center bg-white">
-            <Text>Weather App</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
+    return <Navigation />;
 }
---
+++ Weather App/screens/Home.js
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from "react-native";
+
+function Home(props) {
+    return (
+        <SafeAreaView>
+            <Text>Home</Text>
+        </SafeAreaView>
+    );
+}
+
+export default Home;
